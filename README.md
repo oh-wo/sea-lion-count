c0bdd65192230fb18379d7919d50f7950080f2d7
owen Bodley <owen@biomatters.com>
2017-04-02 23:32:12 +1200

    master
     * Initial peak is working

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ Learning machine learning by entering https://www.kaggle.com/c/noaa-fisheries-st
 
 # Installation
 * Download data from the [Kaggle project page](https://www.kaggle.com/c/noaa-fisheries-steller-sea-lion-population-count/data) and save it to `/input`
+* If you haven't already, `sudo pip install virtualenv` ([see here for info](https://www.dabapps.com/blog/introduction-to-pip-and-virtualenv-python/))
+  * `virtualenv env`
+* `env/bin/pip install -r requirements.txt` ([see here for info](http://stackoverflow.com/a/15593865/2152821))
--- initial-peak.py
+++ initial-peak.py
@@ -1,12 +1,25 @@
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+# linear algebra
+import numpy as np
+# data processing, CSV file I/O (e.g. pd.read_csv)
+import pandas as pd
 from subprocess import check_output
 import matplotlib.pyplot as plt
-print(check_output(["ls", "../input"]).decode("utf8"))
+print(check_output(["ls", "./input"]).decode("utf8"))
 from glob import glob
 import seaborn as sns
 from scipy import stats
 
-df = pd.read_csv('../input/Train/train.csv')
+# pd.set_option(max_columns=10)
+
+df = pd.read_csv('./input/Train/train.csv')
 print("{} training samples total".format(df.shape[0]))
 df.head()
+
+# Prepare a graph of the data.
+df[['adult_males', 'subadult_males', 'adult_females', 'juveniles', 'pups']]\
+    .sum(axis=0)\
+    .plot\
+    .barh()
+
+# Actually show the graph.
+plt.show()
